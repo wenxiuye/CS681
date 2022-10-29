57775fa0926bc69fd5c78083884fe86e93110bf3
wenxiuye <33331792+wenxiuye@users.noreply.github.com>
2022-10-29 00:51:18 -0400

    Add files via upload

---
+++ hw6/src/edu/umb/cs681/hw06/Manhattan.java
@@ -0,0 +1,14 @@
+package edu.umb.cs681.hw06;
+
+import java.util.List;
+
+public class Manhattan implements DistanceMetric{
+    public double distance(List<Double> p1, List<Double> p2) {
+        // TODO Error handling if p1.size() != p2.size()
+        double sumOfDifference = 0.0;
+        for(int i=0; i < p1.size(); i++) {
+            sumOfDifference += Math.abs( p1.get(i)-p2.get(i));
+        }
+        return Math.sqrt(sumOfDifference);      
+    }
+}
